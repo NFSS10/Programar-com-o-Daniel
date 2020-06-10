c8cf47f06a4d5bd5651376d64624154f057c71a6
dfobastos <66648237+dfobastos@users.noreply.github.com>
2020-06-10 16:52:40 +0100

    TicTacToe
    As alterações que fiz neste projecto foi retirar o "1" no random.randrange e colocar "2" para que assim seja possivel gerar cara ou coroa corretamente

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,13 +4,12 @@ import random
 
 TicTacToe
 
-
 """
 
 player1 = input("Nome primeiro jogador: ")
 player2 = input("Nome do segundo jogador: ")
 
-lancamento = random.randrange(0,1)
+lancamento = random.randrange(0,2) # Fiz alteracao do 1 para 2 para que ao correr o random o numero "1" tambem surgir
 
 
 if lancamento == 0:
@@ -24,10 +23,9 @@ else:
 print(f'{player1} é {player_coin1}')
 print(f'{player2} é {player_coin2}')
 
-lancamento2 = random.randrange(0,1)
+lancamento2 = random.randrange(0,2) # Fiz alteracao do 1 para 2 para que ao correr o random o numero "1" tambem surgir
 
 if lancamento2 == 0:
     print('Começa primeiro o jogador que saiu cara')
 else:
     print('Começa primeiro o jogador que saiu coroa')
-
